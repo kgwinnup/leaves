71f5ff097bc873c206db3ad511778323d3e80236
kgwinnup <kgwinnup@vmware.com>
2020-05-03 17:12:49 -0400

    changed softprob to softmax because that is how it was implemented

--- xgensemble_io.go
+++ xgensemble_io.go
@@ -212,8 +212,8 @@ func XGEnsembleFromReader(reader *bufio.Reader, loadTransformation bool) (*Ensem
 	if loadTransformation {
 		if header.NameObj == "binary:logistic" {
 			transform = &transformation.TransformLogistic{}
-		} else if header.NameObj == "multi:softprob" {
-			transform = &transformation.TransformSoftprob{}
+		} else if header.NameObj == "multi:softmax" {
+			transform = &transformation.TransformSoftmax{}
 		} else {
 			return nil, fmt.Errorf("unknown transformation function '%s'", header.NameObj)
 		}
